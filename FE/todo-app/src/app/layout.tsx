fb759dfcb1cd071332129e0aaa3c9908bf51da72
Patrik93JS <99814207+Patrik93JS@users.noreply.github.com>
2023-05-31 17:35:59 +0200

    test

--- FE/todo-app/src/app/layout.tsx
+++ FE/todo-app/src/app/layout.tsx
@@ -4,13 +4,34 @@ import { Inter } from "next/font/google";
 
 import { ReactNode, useState } from "react";
 import { useRouter } from "next/navigation";
-
-const inter = Inter({ subsets: ["latin"] });
+import Head from "next/head";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "../../components/ui/navigation-menu";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <>
-      <body> {children}</body>
-    </>
+    <html lang="en">
+      <body className="text-indigo-500">
+        <NavigationMenu className="bg-black">
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <NavigationMenuLink>Link</NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+        <div className="text-gray-700">{"ahoj"}</div>
+        <p className="text-sky-400">The quick brown fox...</p>
+        <div>{children}</div>
+      </body>
+    </html>
   );
 }
