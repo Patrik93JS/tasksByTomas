2d4e399853ccc9384848ed93513be3bf06a5a401
Patrik93JS <99814207+Patrik93JS@users.noreply.github.com>
2023-05-31 22:02:20 +0200

    search icon dodelavka

--- FE/todo-app/src/components/SideBar.tsx
+++ FE/todo-app/src/components/SideBar.tsx
@@ -1,6 +1,5 @@
 import React, { ReactNode } from "react";
 import { BsPlus, BsCheckLg, BsCollection } from "react-icons/bs";
-import { FaFire, FaPoo } from "react-icons/fa";
 
 export default function SideBar() {
   return (
--- FE/todo-app/src/components/TopNavigation.tsx
+++ FE/todo-app/src/components/TopNavigation.tsx
@@ -13,7 +13,7 @@ export default function TopNavigation() {
 const Search = () => (
   <div className="search">
     <input className="search-input" type="text" placeholder="Search..." />
-    <FaSearch size="18" className="text-secondary my-auto" />
+    <FaSearch size="18" className="search-icon my-auto" />
   </div>
 );
 
