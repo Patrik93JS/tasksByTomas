46790b485f100e0070e0b32476a414f11d68b2be
Patrik93JS <99814207+Patrik93JS@users.noreply.github.com>
2023-06-13 15:26:35 +0200

    arrow funkce

--- FE/todo-app/src/hooks/useModal.tsx
+++ FE/todo-app/src/hooks/useModal.tsx
@@ -3,17 +3,11 @@ import { useState } from "react";
 export const useModal = () => {
   const [isOpen, setOpen] = useState(false);
 
-  const open = () => {
-    setOpen(true);
-  };
+  const open = () => setOpen(true);
 
-  const close = () => {
-    setOpen(false);
-  };
+  const close = () => setOpen(false);
 
-  const toggle = () => {
-    setOpen((prevOpen) => !prevOpen);
-  };
+  const toggle = () => setOpen((prevOpen) => !prevOpen);
 
   return { isOpen, open, close, toggle };
 };
