0fb102da92ca763fe273016446437a1a82dd0935
Patrik93JS <99814207+Patrik93JS@users.noreply.github.com>
2023-06-21 12:50:07 +0200

    odstraneni komentu

--- FE/todo-app/src/store/reducers.ts
+++ FE/todo-app/src/store/reducers.ts
@@ -2,11 +2,9 @@ import { combineReducers } from "@reduxjs/toolkit";
 import { appApi } from "./api";
 import { createGroupToDo } from "./slices/createGroupToDo";
 import { tokenSlice } from "./slices/tokenSlice";
-// import { authSlice } from "./slices/authSlice";
 
 export const appReducer = combineReducers({
   [appApi.reducerPath]: appApi.reducer,
   [createGroupToDo.name]: createGroupToDo.reducer,
   [tokenSlice.name]: tokenSlice.reducer,
-  // [authSlice.name]: authSlice.reducer,
 });
