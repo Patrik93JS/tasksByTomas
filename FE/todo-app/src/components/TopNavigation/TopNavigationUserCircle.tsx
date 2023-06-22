fcc51d6d231c865a13b1247889e1bbd4d4c8080c
Patrik93JS <99814207+Patrik93JS@users.noreply.github.com>
2023-06-22 11:55:20 +0200

    logout jako funkce a ne jako page

--- FE/todo-app/src/components/TopNavigation/TopNavigationUserCircle.tsx
+++ FE/todo-app/src/components/TopNavigation/TopNavigationUserCircle.tsx
@@ -1,16 +1,25 @@
-import { FC } from "react";
+import { FC, useCallback } from "react";
 import { FaUserCircle } from "react-icons/fa";
 import styles from "./TopNavigation.module.css";
 import { useModal } from "@/hooks/useModal";
 import { useMeQuery } from "@/store/api/authenticationApi";
 import { useRouter } from "next/navigation";
-import Link from "next/link";
+import { useAppDispatch } from "@/store/hooks";
+import { tokenSlice } from "@/store/slices/tokenSlice";
+import { appApi } from "@/store/api";
 
 export const UserCircle: FC = () => {
   const { isOpen, toggle } = useModal();
 
   const data = useMeQuery();
   const router = useRouter();
+  const dispatch = useAppDispatch();
+
+  const logout = useCallback(async () => {
+    dispatch(tokenSlice.actions.reset());
+    dispatch(appApi.util.resetApiState());
+    await router.push("/login");
+  }, [dispatch, router]);
 
   return (
     <>
@@ -22,9 +31,9 @@ export const UserCircle: FC = () => {
           <div className="border-b mx-10">
             <div className={`${styles.username} flex justify-center px-4 pb-2`}>{data.data?.username}</div>
           </div>
-          <Link className={styles.logoutButton} href="/login">
+          <button className={styles.logoutButton} onClick={logout}>
             Logout
-          </Link>
+          </button>
         </div>
       )}
     </>
