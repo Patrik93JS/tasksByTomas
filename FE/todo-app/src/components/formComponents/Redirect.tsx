b995ea5ee9904c772821eabfc16a953dd9c20d90
Patrik93JS <99814207+Patrik93JS@users.noreply.github.com>
2023-06-28 08:55:20 +0200

    link cn funkce

--- FE/todo-app/src/components/formComponents/Redirect.tsx
+++ FE/todo-app/src/components/formComponents/Redirect.tsx
@@ -1,6 +1,7 @@
 import React, { FC, ReactNode } from "react";
 import Link, { LinkProps } from "next/link";
 import styles from "./Components.module.css";
+import { cn } from "@/lib/utils";
 
 type Props = LinkProps & {
   children?: ReactNode;
@@ -9,8 +10,10 @@ type Props = LinkProps & {
 
 export const Redirect: FC<Props> = (props: Props) => {
   const { children, className, ...rest } = props;
+  const linkClass = cn(`${styles.redirect} ${className}`);
+
   return (
-    <Link className={`${styles.redirect} ${className}`} {...rest}>
+    <Link className={linkClass} {...rest}>
       {children}
     </Link>
   );
