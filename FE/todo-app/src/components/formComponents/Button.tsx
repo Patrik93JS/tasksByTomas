67d0378374e1ace36379e7bdb02e88ca08063225
Patrik93JS <99814207+Patrik93JS@users.noreply.github.com>
2023-06-28 08:01:48 +0200

    pouziti cn funkce

--- FE/todo-app/src/components/formComponents/Button.tsx
+++ FE/todo-app/src/components/formComponents/Button.tsx
@@ -1,20 +1,19 @@
-import React, { FC, ReactNode } from "react";
+import React, { ComponentPropsWithoutRef, FC, ReactNode } from "react";
 import styles from "./Components.module.css";
 import { AiFillCloseCircle } from "react-icons/ai";
+import { cn } from "@/lib/utils";
 
-type Props = React.ComponentPropsWithoutRef<"button"> & {
+type Props = ComponentPropsWithoutRef<"button"> & {
   buttonType?: "closeButton" | "submitType";
   children?: ReactNode;
 };
 
 export const Button: FC<Props> = (props: Props) => {
   const { buttonType, children, ...rest } = props;
+  const buttonClass = cn(buttonType === "closeButton" ? styles.closeButton : styles.submitButton);
+
   return (
-    <button
-      className={buttonType === "closeButton" ? styles.closeButton : styles.submitButton}
-      type={buttonType === "submitType" ? "submit" : undefined}
-      {...rest}
-    >
+    <button className={buttonClass} type={buttonType === "submitType" ? "submit" : undefined} {...rest}>
       {buttonType === "closeButton" ? <AiFillCloseCircle /> : children}
     </button>
   );
