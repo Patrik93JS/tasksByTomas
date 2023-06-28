e2cd954d234e52f0a31d673c129df9abe7cfcffb
Patrik93JS <99814207+Patrik93JS@users.noreply.github.com>
2023-06-28 08:03:52 +0200

    p tag props

--- FE/todo-app/src/components/formComponents/Description.tsx
+++ FE/todo-app/src/components/formComponents/Description.tsx
@@ -1,10 +1,15 @@
-import React, { FC, ReactNode } from "react";
+import React, { ComponentPropsWithoutRef, FC, ReactNode } from "react";
 
-type Props = {
+type Props = ComponentPropsWithoutRef<"p"> & {
   children: ReactNode;
   className?: string;
 };
 
-export const Description: FC<Props> = ({ children, className }) => {
-  return <p className={`pb-2 ${className}`}>{children}</p>;
+export const Description: FC<Props> = (props: Props) => {
+  const { children, className, ...rest } = props;
+  return (
+    <p className={`pb-2 ${className}`} {...rest}>
+      {children}
+    </p>
+  );
 };
