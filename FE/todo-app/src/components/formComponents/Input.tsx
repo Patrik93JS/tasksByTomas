71a7fdae051c582ef8405321dd3784909a0e1a33
Patrik93JS <99814207+Patrik93JS@users.noreply.github.com>
2023-06-28 12:53:32 +0200

    input oprava

--- FE/todo-app/src/components/formComponents/Input.tsx
+++ FE/todo-app/src/components/formComponents/Input.tsx
@@ -5,22 +5,18 @@ import { cn } from "@/lib/utils";
 
 type Props = ComponentPropsWithoutRef<"input"> & {
   name: string;
-  type: string;
   description?: string;
-  placeholder?: string;
   validationValue?: RegExp;
   validationMessage?: string;
 };
 
-export const Input: FC<Props> = ({ name, description, placeholder, type, validationValue, validationMessage, ...rest }) => {
+export const Input: FC<Props> = ({ name, description, validationValue, validationMessage, ...rest }) => {
   const { register } = useFormContext();
   const inputStyle = cn("bg-black px-3 py-1 rounded-md text-green-500");
   return (
     <div className="p-6 flex justify-center items-center flex-col">
       {description && <Description>{description}</Description>}
       <input
-        type={type}
-        placeholder={placeholder}
         className={inputStyle}
         {...register(`${name}`, {
           pattern:
