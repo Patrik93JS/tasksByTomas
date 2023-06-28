6b3ace4feb43d6f17afb97fa8f49442f5354e183
Patrik93JS <99814207+Patrik93JS@users.noreply.github.com>
2023-06-28 09:56:05 +0200

    input props

--- FE/todo-app/src/components/formComponents/Input.tsx
+++ FE/todo-app/src/components/formComponents/Input.tsx
@@ -1,28 +1,28 @@
-import React, { FC } from "react";
+import React, { ComponentPropsWithoutRef, FC } from "react";
 import { useFormContext } from "react-hook-form";
 import { Description } from "./Description";
+import { cn } from "@/lib/utils";
 
-type Props = {
+type Props = ComponentPropsWithoutRef<"input"> & {
   name: string;
   type: string;
   description?: string;
   placeholder?: string;
   validationValue?: RegExp;
   validationMessage?: string;
-  className?: string;
-  ariaLabel?: string;
 };
 
-export const Input: FC<Props> = ({ name, description, placeholder, type, validationValue, validationMessage, className, ariaLabel }) => {
+export const Input: FC<Props> = (props: Props) => {
+  const { name, description, placeholder, type, validationValue, validationMessage, ...rest } = props;
   const { register } = useFormContext();
+  const inputStyle = cn("bg-black px-3 py-1 rounded-md text-green-500");
   return (
     <div className="p-6 flex justify-center items-center flex-col">
       {description && <Description>{description}</Description>}
       <input
-        aria-label={ariaLabel}
         type={type}
         placeholder={placeholder}
-        className={`bg-black px-3 py-1 rounded-md text-green-500 ${className}`}
+        className={inputStyle}
         {...register(`${name}`, {
           pattern:
             validationValue && validationMessage
@@ -33,6 +33,7 @@ export const Input: FC<Props> = ({ name, description, placeholder, type, validat
               : undefined,
           required: `${name} is required`,
         })}
+        {...rest}
       />
     </div>
   );
