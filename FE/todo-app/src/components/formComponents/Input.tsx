79c2dd71216f5ac8612b2f6082d7c28430f79905
Patrik93JS <99814207+Patrik93JS@users.noreply.github.com>
2023-06-28 14:42:18 +0200

    uprava input

--- FE/todo-app/src/components/formComponents/Input.tsx
+++ FE/todo-app/src/components/formComponents/Input.tsx
@@ -13,19 +13,16 @@ type Props = ComponentPropsWithoutRef<"input"> & {
 export const Input: FC<Props> = ({ name, description, validationValue, validationMessage, ...rest }) => {
   const { register } = useFormContext();
   const inputStyle = cn("bg-black px-3 py-1 rounded-md text-green-500");
+
+  const pattern = validationValue && validationMessage ? { value: validationValue, message: validationMessage } : undefined;
+
   return (
     <div className="p-6 flex justify-center items-center flex-col">
       {description && <Description>{description}</Description>}
       <input
         className={inputStyle}
         {...register(`${name}`, {
-          pattern:
-            validationValue && validationMessage
-              ? {
-                  value: validationValue,
-                  message: validationMessage,
-                }
-              : undefined,
+          pattern,
           required: `${name} is required`,
         })}
         {...rest}
