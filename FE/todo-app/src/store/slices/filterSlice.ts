7ba8492269d113b1a7e2e57edf54a2348ce8e48a
Patrik93JS <99814207+Patrik93JS@users.noreply.github.com>
2023-07-03 15:17:06 +0200

    filter slice

---
+++ FE/todo-app/src/store/slices/filterSlice.ts
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+type Filter = {
+  filter: string;
+};
+
+export const filterSlice = createSlice({
+  name: "filter",
+  initialState: {
+    filter: "",
+  } as Filter,
+  reducers: {
+    setFilter: (state, action) => {
+      state.filter = action.payload;
+    },
+  },
+});
+
+export const { setFilter } = filterSlice.actions;
+export default filterSlice.reducer;
