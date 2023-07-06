8e49f84dfaff756b1cfc2e2ba1537ba476e24c1d
Patrik93JS <99814207+Patrik93JS@users.noreply.github.com>
2023-07-06 17:04:08 +0200

    edit long desc

--- FE/todo-app/src/components/todo/EditToDo.tsx
+++ FE/todo-app/src/components/todo/EditToDo.tsx
@@ -1,15 +1,54 @@
-import React, { useState } from "react";
+import React, { FC, useState } from "react";
 import styles from "./ToDo.module.css";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { toolbarOptions } from "./toolbarOptions";
+import { UpdateToDoRequest, to_do } from "@/types/ToDo";
+import { useUpdateToDoMutation } from "@/store/api/todoApi";
+import { Button } from "../formComponents/Button";
 
-export const EditToDo = () => {
+type Props = {
+  todo: to_do;
+};
+
+export const EditToDo: FC<Props> = ({ todo }) => {
   const [value, setValue] = useState("");
+  const [edit, setEdit] = useState(false);
+  const [update] = useUpdateToDoMutation();
+
+  const handleChange = (content: string) => {
+    setValue(content);
+  };
+
+  const handleEdit = () => {
+    setEdit(true);
+  };
+
+  const handleSave = () => {
+    const updateData: UpdateToDoRequest = {
+      id: todo.id,
+      title: todo.attributes.title,
+      description: todo.attributes.description,
+      longDescription: value,
+      mustBeCompleted: todo.attributes.mustBeCompleted,
+      completed: todo.attributes.completed,
+      to_do_group: todo.attributes.to_do_group.data.id,
+    };
+    update(updateData);
+    setEdit(false);
+  };
 
   return (
-    <div className={styles.editToDo}>
-      <ReactQuill theme="snow" modules={toolbarOptions} value={value} onChange={setValue} />
-    </div>
+    <>
+      <div className={styles.editToDo}>
+        <div>
+          <ReactQuill theme="snow" modules={toolbarOptions} value={value} onChange={handleChange} readOnly={!edit} />
+        </div>
+        <div className="pt-5">
+          {!edit && <Button onClick={handleEdit}>Edit</Button>}
+          {edit && <Button onClick={handleSave}>Uložit</Button>}
+        </div>
+      </div>
+    </>
   );
 };
--- FE/todo-app/src/components/todo/ToDoModal.tsx
+++ FE/todo-app/src/components/todo/ToDoModal.tsx
@@ -11,20 +11,19 @@ type Props = {
 };
 
 export const ToDoModal: FC<Props> = ({ close, todo }) => {
-  console.log(todo.attributes.longDescription);
+  console.log("rich", todo.attributes.longDescription);
   return (
     <div className={styles.toDoModalContainer}>
-      <div className="bg-gray-800 w-1/2 h-1/2 ">
+      <div className="bg-gray-800 w-1/2 h-auto ">
         <div className="flex justify-end  p-3">
           <Button onClick={close} buttonType="closeButton" />
         </div>
         <div className="flex justify-center p-5 flex-col text-center">
           <Description>{todo.attributes.title}</Description>
           <Description>{todo.attributes.description}</Description>
-          <Description>{todo.attributes.longDescription}</Description>
+          <div dangerouslySetInnerHTML={{ __html: todo.attributes.longDescription }}></div>
+          <EditToDo todo={todo} />
         </div>
-
-        {todo.attributes.longDescription ? todo.attributes.longDescription : <EditToDo />}
       </div>
     </div>
   );
